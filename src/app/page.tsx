55452c90e44be15f277ec5d6f1690091b02655d9
Boram Kim <boram2445@gmail.com>
2023-07-20 20:27:57 +0900

    Design: 홈 프로필 UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import Image from 'next/image';
+import ProfileImg from '../../public/images/profile.jpg';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      안녕하세요
-    </main>
+    <>
+      <section className='text-center py-5 bg-white-brown'>
+        <Image
+          src={ProfileImg}
+          alt='프로파일 이미지'
+          width={200}
+          height={200}
+          className='rounded-full mx-auto'
+        />
+        <h3 className='my-2 text-xl font-semibold'>Hi, I&apos;m Boram</h3>
+        <p>
+          Frontend engineer
+          <br />
+          즐겁게 코딩하는 코더 Boram 블로그
+        </p>
+        {/* 버튼을 Link로 감싸야 할까 */}
+        <button className='my-2.5 py-1.5 px-6 border-stone-300 border-2 rounded-xl text-sm ease-in hover:brightness-125 '>
+          <Link href='/contact'>Contact Me</Link>
+        </button>
+      </section>
+    </>
   );
 }
