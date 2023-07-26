0a1c8a01e88928181a5329698aec5afbbb631d7a
Boram Kim <boram2445@gmail.com>
2023-07-26 23:21:59 +0900

    Refactor: 모든 포스트 불러오는 함수 캐싱

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { promises as fs } from 'fs';
+import { cache } from 'react';
 
 export type Post = {
   title: string;
@@ -16,13 +17,13 @@ export type PostData = Post & {
   next: Post | null;
 };
 
-export async function getAllPosts(): Promise<Post[]> {
+export const getAllPosts = cache(async () => {
   const filePath = path.join(process.cwd(), 'data', 'posts.json');
   return await fs
     .readFile(filePath, 'utf-8')
     .then<Post[]>(JSON.parse)
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
-}
+});
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const posts = await getAllPosts();
