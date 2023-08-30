c91f2b66276b19781f98855616f510adfe2116a0
Boram Kim <boram2445@gmail.com>
2023-08-30 23:32:16 +0900

    Refactor: comment hook 함수 캐싱

---
+++ src/hooks/useComment.ts
@@ -0,0 +1,75 @@
+import useSWR from 'swr';
+import axios from 'axios';
+import { Comment } from '@/service/comment';
+import { useSWRConfig } from 'swr';
+import { useCallback } from 'react';
+
+export default function useComment(postId: string) {
+  const {
+    data: comments,
+    isLoading,
+    error,
+  } = useSWR<Comment[]>(`/api/comment/${postId}`);
+  const { mutate } = useSWRConfig();
+
+  const setComment = useCallback(
+    (isLoggedInUser: boolean, data: PostGuestComment | PostUserComment) => {
+      const postData = {
+        ...data,
+        type: isLoggedInUser ? 'loggedInUserComment' : 'guestComment',
+      };
+
+      axios
+        .post(`/api/comment/${postId}`, postData)
+        .then(() => mutate(`/api/comment/${postId}`));
+    },
+    [mutate, postId]
+  );
+
+  const deleteComment = useCallback(
+    (commentId: string, parentCommentId?: string) => {
+      let url = `/api/comment/${postId}?commentId=${commentId}${
+        parentCommentId ? `&parentCommentId=${parentCommentId}` : ''
+      }`;
+
+      axios
+        .delete(url) //
+        .then(() => mutate(`/api/comment/${postId}`));
+    },
+    [mutate, postId]
+  );
+
+  const checkPassword = useCallback(
+    async (data: PostPassword) => {
+      return await axios.post(`/api/comment/${postId}/password`, data);
+    },
+    [postId]
+  );
+
+  return {
+    comments,
+    isLoading,
+    error,
+    setComment,
+    deleteComment,
+    checkPassword,
+  };
+}
+
+type PostGuestComment = {
+  text: string;
+  name: string;
+  password: string;
+  commentId?: string;
+};
+
+type PostUserComment = {
+  text: string;
+  commentId?: string;
+};
+
+type PostPassword = {
+  parentCommentId?: string;
+  commentId: string;
+  password: string;
+};
