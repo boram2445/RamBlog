5331a606d99413f5ab591b369ff13a23700c54f1
Boram Kim <boram2445@gmail.com>
2023-09-30 21:30:57 +0900

    Feat: loading 페이지 스켈레톤 추가

--- src/app/[user]/(home)/about/loading.tsx
+++ src/app/[user]/(home)/about/loading.tsx
@@ -1,3 +1,14 @@
+import Skeleton from '@/components/ui/Skeleton';
+import Title from '@/components/ui/Title';
+
 export default function Loading() {
-  return <p>About 준비중</p>;
+  return (
+    <>
+      <div className='mb-6'>
+        <Title title='About me' />
+      </div>
+      <Skeleton className='w-1/2 h-[1.5rem]' />
+      <Skeleton className='mt-3 w-2/3 h-[1.5rem]' />
+    </>
+  );
 }
